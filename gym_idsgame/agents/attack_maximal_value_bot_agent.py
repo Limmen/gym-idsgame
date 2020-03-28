1af2fa9784c08bf2cb241bb598c06b7d14c25716
Kim Hammar <kimham@kth.se>
2020-03-28 10:30:06 +0100

    bugfix

--- gym_idsgame/agents/attack_maximal_value_bot_agent.py
+++ gym_idsgame/agents/attack_maximal_value_bot_agent.py
@@ -32,13 +32,16 @@ class AttackMaximalValueBotAgent(BotAgent):
         actions = list(range(self.game_config.num_actions))
         legal_actions = list(filter(lambda action: idsgame_util.is_attack_id_legal(action, self.game_config,
                                                                                    game_state.attacker_pos), actions))
+        attacker_row, attacker_col = game_state.attacker_pos
         max_node_value = float("-inf")
         max_action_id = -1
         for id, node in enumerate(self.game_config.network_config.node_list):
             if node == NodeType.SERVER.value or node == NodeType.DATA.value:
                 max_idx = np.argmax(game_state.attack_values[id])
                 action_id = idsgame_util.get_action_id(id, max_idx, self.game_config)
-                if game_state.attack_values[id][max_idx] > max_node_value and action_id in legal_actions:
+                node_row, node_col = self.game_config.network_config.get_node_pos(id)
+                if game_state.attack_values[id][max_idx] > max_node_value and action_id in legal_actions and \
+                        node_row < attacker_row:
                     max_node_value = game_state.attack_values[id][max_idx]
                     max_action_id = action_id
         if max_action_id == -1:
