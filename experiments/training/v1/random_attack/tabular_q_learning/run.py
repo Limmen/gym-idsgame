838c1d2c1da2a0f96df880302d9f5d77cf3958cd
Kim Hammar <kimham@kth.se>
2020-03-30 12:16:17 +0200

    retrain and upd results

--- experiments/training/v1/random_attack/tabular_q_learning/run.py
+++ experiments/training/v1/random_attack/tabular_q_learning/run.py
@@ -28,12 +28,12 @@ def default_config() -> ClientConfig:
     """
     :return: Default configuration for the experiment
     """
-    q_agent_config = QAgentConfig(gamma=0.99, alpha=0.3, epsilon=1, render=False, eval_sleep=0.9,
-                                  min_epsilon=0.1, eval_episodes=1, train_log_frequency=100,
-                                  epsilon_decay=0.99, video=True, eval_log_frequency=1,
-                                  video_fps=5, video_dir=default_output_dir() + "/videos", num_episodes=1000,
+    q_agent_config = QAgentConfig(gamma=0.99, alpha=0.05, epsilon=1, render=False, eval_sleep=0.9,
+                                  min_epsilon=0.01, eval_episodes=100, train_log_frequency=100,
+                                  epsilon_decay=0.999, video=True, eval_log_frequency=1,
+                                  video_fps=5, video_dir=default_output_dir() + "/videos", num_episodes=5000,
                                   eval_render=False, gifs=True, gif_dir=default_output_dir() + "/gifs",
-                                  eval_frequency=100, attacker=False, defender=True,
+                                  eval_frequency=1000, attacker=False, defender=True, video_frequency=101,
                                   save_dir=default_output_dir() + "/data")
     env_name = "idsgame-random_attack-v1"
     client_config = ClientConfig(env_name=env_name, defender_type=AgentType.TABULAR_Q_AGENT.value,
