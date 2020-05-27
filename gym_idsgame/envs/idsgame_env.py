8d54dc30a34f6f76ae608271e2fe59c6db218495
Kim Hammar <kimham@kth.se>
2020-05-27 10:03:07 +0200

    test

--- gym_idsgame/envs/idsgame_env.py
+++ gym_idsgame/envs/idsgame_env.py
@@ -3664,9 +3664,13 @@ class IdsGameMinimalDefenseV18Env(AttackerEnv):
         :param idsgame_config: configuration of the environment (if not specified a default config is used)
         """
         if idsgame_config is None:
-            game_config = GameConfig(num_layers=1, num_servers_per_layer=1, num_attack_types=8, max_value=8,
-                                     min_random_a_val=0, min_random_d_val=8, min_random_det_val=1)
-            game_config.set_initial_state(defense_val=8, attack_val=0, num_vulnerabilities_per_node=1, det_val=1,
+            # game_config = GameConfig(num_layers=1, num_servers_per_layer=1, num_attack_types=8, max_value=8,
+            #                          min_random_a_val=0, min_random_d_val=8, min_random_det_val=1)
+            # game_config.set_initial_state(defense_val=8, attack_val=0, num_vulnerabilities_per_node=1, det_val=1,
+            #                               vulnerability_val=0, num_vulnerabilities_per_layer=1)
+            game_config = GameConfig(num_layers=1, num_servers_per_layer=1, num_attack_types=4, max_value=8,
+                                     min_random_a_val=0, min_random_d_val=8, min_random_det_val=2)
+            game_config.set_initial_state(defense_val=8, attack_val=0, num_vulnerabilities_per_node=1, det_val=3,
                                           vulnerability_val=0, num_vulnerabilities_per_layer=1)
             game_config.dense_rewards_v2 = True
             game_config.network_config.fully_observed = False
