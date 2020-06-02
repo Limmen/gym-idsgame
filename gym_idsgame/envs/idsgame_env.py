2f576a56a563e77c5e1144893aad1b5b46ac1911
Kim Hammar <kimham@kth.se>
2020-06-03 00:58:42 +0200

    bugfix

--- gym_idsgame/envs/idsgame_env.py
+++ gym_idsgame/envs/idsgame_env.py
@@ -4156,10 +4156,10 @@ class IdsGameMinimalDefenseV19Env(AttackerEnv):
             idsgame_config.local_view_observations = True
             idsgame_config.reconnaissance_bool_features = True
             idsgame_config.reconnaissance_actions = True
-            idsgame_config.reconnaissance_reward = False
+            idsgame_config.reconnaissance_reward = True
             idsgame_config.randomize_visibility = True
             idsgame_config.visibility_p = 0.25
-            idsgame_config.reconnaissance_detection_factor = 0.1
+            idsgame_config.reconnaissance_detection_factor = 1
         super().__init__(idsgame_config=idsgame_config, save_dir=save_dir)
 
 
