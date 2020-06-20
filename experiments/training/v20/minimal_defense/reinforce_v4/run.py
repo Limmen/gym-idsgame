925d655162723d2400864ce3ee960d3cfe3096a8
Kim Hammar <kimham@kth.se>
2020-06-20 19:28:17 +0200

    test

--- experiments/training/v20/minimal_defense/reinforce_v4/run.py
+++ experiments/training/v20/minimal_defense/reinforce_v4/run.py
@@ -51,37 +51,35 @@ def default_config() -> ClientConfig:
     """
     :return: Default configuration for the experiment
     """
-    pg_agent_config = PolicyGradientAgentConfig(gamma=0.999, alpha_attacker=0.00001,
-                                                alpha_defender=0.00001, epsilon=1, render=False, eval_sleep=0.9,
-                                                min_epsilon=0.01, eval_episodes=100, train_log_frequency=10,
+    pg_agent_config = PolicyGradientAgentConfig(gamma=0.999, alpha_attacker=0.0001, epsilon=1, render=False, eval_sleep=0.9,
+                                                min_epsilon=0.01, eval_episodes=100, train_log_frequency=1,
                                                 epsilon_decay=0.9999, video=True, eval_log_frequency=1,
                                                 video_fps=5, video_dir=default_output_dir() + "/results/videos",
-                                                num_episodes=200000001,
+                                                num_episodes=10000,
                                                 eval_render=False, gifs=True,
                                                 gif_dir=default_output_dir() + "/results/gifs",
-                                                eval_frequency=10000000000, attacker=False, defender=True, video_frequency=101,
+                                                eval_frequency=1000000000, attacker=True, defender=False, video_frequency=101,
                                                 save_dir=default_output_dir() + "/results/data",
                                                 checkpoint_freq=100,
                                                 input_dim_attacker=((4 + 2) * 4),
                                                 output_dim_attacker=(4 + 1) * 4,
                                                 input_dim_defender=((4 + 1) * 4),
                                                 output_dim_defender=5 * 4,
-                                                hidden_dim=128, num_hidden_layers=2,
-                                                batch_size=64,
-                                                gpu=False, tensorboard=True,
+                                                hidden_dim=128,
+                                                num_hidden_layers=2, batch_size=64,
+                                                gpu=True, tensorboard=True,
                                                 tensorboard_dir=default_output_dir() + "/results/tensorboard",
                                                 optimizer="Adam", lr_exp_decay=False, lr_decay_rate=0.999,
                                                 state_length=1, normalize_features=False, merged_ad_features=True,
                                                 zero_mean_features=False, gpu_id=0, lstm_network=False,
-                                                lstm_seq_length=4, num_lstm_layers=2
-                                                )
-    env_name = "idsgame-maximal_attack-v19"
-    client_config = ClientConfig(env_name=env_name, defender_type=AgentType.REINFORCE_AGENT.value,
-                                 mode=RunnerMode.TRAIN_DEFENDER.value,
+                                                lstm_seq_length=4, num_lstm_layers=2)
+    env_name = "idsgame-minimal_defense-v20"
+    client_config = ClientConfig(env_name=env_name, attacker_type=AgentType.REINFORCE_AGENT.value,
+                                 mode=RunnerMode.TRAIN_ATTACKER.value,
                                  pg_agent_config=pg_agent_config, output_dir=default_output_dir(),
-                                 title="AttackMaximalAttacker vs REINFORCE",
-                                 run_many=False, random_seeds=[0, 999, 299, 399, 499], random_seed=0)
-    client_config = hp_tuning_config(client_config)
+                                 title="REINFORCE vs DefendMinimalDefender",
+                                 run_many=False, random_seeds=[0, 999, 299, 399, 499], random_seed=399)
+    #client_config = hp_tuning_config(client_config)
     return client_config
 
 def write_default_config(path:str = None) -> None:
@@ -146,7 +144,7 @@ def run_experiment(configpath: str, random_seed: int, noconfig: bool):
         config = default_config()
     time_str = str(time.time())
     util.create_artefact_dirs(config.output_dir, random_seed)
-    logger = util.setup_logger("maximal_attack_vs_reinforce-v19", config.output_dir + "/results/logs/" +
+    logger = util.setup_logger("reinforce_vs_minimal_defense-v20", config.output_dir + "/results/logs/" +
                                str(random_seed) + "/",
                                time_str=time_str)
     config.pg_agent_config.save_dir = default_output_dir() + "/results/data/" + str(random_seed) + "/"
@@ -177,7 +175,7 @@ def run_experiment(configpath: str, random_seed: int, noconfig: bool):
 # Program entrypoint
 if __name__ == '__main__':
     args = util.parse_args(default_config_path())
-    experiment_title = "maximal attack vs REINFORCE"
+    experiment_title = "REINFORCE vs minimal defense"
     if args.configpath is not None and not args.noconfig:
         if not os.path.exists(args.configpath):
             write_default_config()
