dd60316bf94057397bbf226dc1d83d548542fbcf
Kim Hammar <kimham@kth.se>
2021-02-01 17:30:21 +0100

    bugfix

--- gym_idsgame/agents/agent.py
+++ gym_idsgame/agents/agent.py
@@ -2,19 +2,19 @@
 Abstract agent for the gym-idsgame environment
 """
 from abc import ABC
-from gym_idsgame.envs.dao.game_config import GameConfig
+#from gym_idsgame.envs.dao.game_config import GameConfig
 
 class Agent(ABC):
     """
     Abstract class representing an agent
     """
 
-    def __init__(self, game_config: GameConfig):
+    def __init__(self, game_config):
         """
         Class constructor
 
         :param game_config: the game configuration
         """
         self.game_config = game_config
-        if self.game_config is None:
-            self.game_config = GameConfig()
+        # if self.game_config is None:
+        #     self.game_config = GameConfig()
